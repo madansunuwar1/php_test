9936c73ff541cb68510978b66773ba049fa22fba
madansunuwar1 <madansun7@gmail.com>
2024-03-27 16:26:44 +0545

    sdfdf

--- api/index.php
+++ api/index.php
@@ -1,14 +1,14 @@
 <?php
+header('Content-Type: app;ication/json');
 
-header('Content-Type: application/json');
-
-if($_SERVER['REQUEST_METHOD'] === 'GET') {
+if ($_SERVER['REQUEST_METHOD'] === 'GET'){
     $response = array(
         'message' => 'Hello, World!'
     );
 
     echo json_encode($response);
-} else {
+}
+else{
     http_response_code(405);
     echo json_encode(array('error' => 'Method Not Allowed'));
 }
